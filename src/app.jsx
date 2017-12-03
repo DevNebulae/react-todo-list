04324e16a33a03d4808346b29a7552e707310607
DevNebulae <gamerkeeperson+github@gmail.com>
2017-12-03 16:03:38 +0100

    Add example of a Redux reducer and store

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,29 @@
 import Input from "./components/input"
 import React, { Component } from "react"
 import Tasks from "./components/tasks"
+import { createStore } from "redux"
+
+const todoReducer = (state = [], { type, payload }) => {
+  switch (type) {
+    case "ADD_TODO":
+      state.push(payload)
+      break
+    case "REMOVE_TODO":
+      state = state.filter(todo => todo.id !== payload)
+      break
+  }
+
+  return state
+}
+
+const store = createStore(todoReducer)
+
+store.subscribe(() => console.log("Store changed: ", store.getState()))
+
+store.dispatch({
+  type: "ADD_TODO",
+  payload: { id: 1, title: "Explore react", done: false }
+})
 
 export default class App extends Component {
   render() {
