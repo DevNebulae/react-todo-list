4570e14cd768375b5506c0544966278d8cac4130
DevNebulae <gamerkeeperson+github@gmail.com>
2017-12-04 21:54:29 +0100

    Convert App component class to function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
 import AddTodo from "../containers/AddTodo"
 import Footer from "./Footer"
 import VisibleTodoList from "../containers/VisibleTodoList"
-import React, { Component } from "react"
+import React from "react"
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <main>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </main>
+)
+
+export default App
